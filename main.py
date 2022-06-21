3110a677959b4285720d58fd4561fb402a0e717b
Azule2 <69428406+Azule2@users.noreply.github.com>
2022-06-21 20:36:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# ------------------------break and continue------------------------
+X = 5 
+
+while X < 15:
+	if x % 2 == 0:
+		print('Even: ', X)
+		break
+	print(X)
+	X += 1
+
+
+while X < 15:
+	if X % 2== 0:
+		print('Even number found')
+		break
+	print(X)
+	X += 2
+
+else:
+	print('All Number were odd ')
+
+
+while X < 15:
+	if X % 2 == 0:
+		print('Even number Found')
+		break
+	print(X)
+	X += 1
+else:
+	print('All number were odd')
